2dda71c8061c6e1a15732f0b518bba52f0ac22ef
raul <roaslin@gmail.com>
2023-09-18 16:42:15 +0200

    Test return error when cant create user

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,29 @@
 import express, { Express, Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import UsersService from './UsersService';
 
 dotenv.config();
 
 const app: Express = express();
+
+const usersService: UsersService = new UsersService();
+
 app.use(cors()).use(express.json()).options('*', cors());
 
-interface NewUserRequest {
+export interface NewUserCommand {
     name: String;
     email: String;
 }
 
-app.post('/users', (req: Request, res: Response) => {
-    const newUserRequest = req.body;
+app.post('/users', async (req: Request, res: Response) => {
+    const newUserRequest: NewUserCommand = req.body;
     if (!newUserRequest.name || !newUserRequest.email) {
         res.statusCode = 400;
         return res.send({ message: 'Invalid payload' });
     }
+
+    const result = await usersService.create(newUserRequest);
     res.send({}).status(201);
 });
 app.get('/users', (req: Request, res: Response) => {
