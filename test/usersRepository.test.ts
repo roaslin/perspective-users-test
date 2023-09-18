5e535883482552e1e2bd5f25e14f5167264f0adf
raul <roaslin@gmail.com>
2023-09-18 18:32:09 +0200

    Test return new user craetion failing

--- test/usersRepository.test.ts
+++ test/usersRepository.test.ts
@@ -14,4 +14,22 @@ describe('UsersRepository should', () => {
         const result = await usersRepository.store(newUserCommand);
         expect(result).toBe('error');
     });
+
+    test('return a new user when can store a user', async () => {
+        const usersRepository: UsersRepository = new UsersRepository();
+        const newUserCommand: NewUserCommand = {
+            name: 'Raul Mordillo LLuva',
+            email: 'raul.test@test.com',
+        };
+
+        const result = await usersRepository.store(newUserCommand);
+
+        const expectedNewUSer = {
+            id: '12345',
+            name: 'Raul Mordillo LLuva',
+            email: 'raul.test@test.com',
+            creationDate: '10/10/2023',
+        };
+        expect(result).toEqual(expectedNewUSer);
+    });
 });
