ea22f270d4b5053d2e84127bf7845bedd1e9c8a5
raul <roaslin@gmail.com>
2023-09-18 15:51:44 +0200

    Test return 400 when payload is invalid

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,18 @@ dotenv.config();
 const app: Express = express();
 app.use(cors()).use(express.json()).options('*', cors());
 
+interface NewUserRequest {
+    name: String;
+    email: String;
+}
 
 app.post('/users', (req: Request, res: Response) => {
-    res.send({ caca: 'cacafuti' }).status(201);
+    const newUserRequest = req.body;
+    if (!newUserRequest.name || !newUserRequest.email) {
+        res.statusCode = 400;
+        return res.send({ message: 'Invalid payload' });
+    }
+    res.send({}).status(201);
 });
 app.get('/users', (req: Request, res: Response) => {
     res.send([]).status(200);
@@ -19,3 +28,5 @@ const port = process.env.PORT || 3111;
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+export default app;
